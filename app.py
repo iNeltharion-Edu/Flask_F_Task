1d6f011a182fb0d982b0078aece69f8d7fe726cf
iNeltharion <an121@ymail.com>
2025-02-11 22:03:19 +0300

    v 0.1

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.get('input_text')
+    return jsonify({'message': f'Вы ввели: {data}'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
